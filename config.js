4903c4ed0650a8d9e83a03c927975eab4d2b0819
nguyenbill <87773607+nguyenbill@users.noreply.github.com>
2021-07-21 23:52:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    botToken:'<Bot Token Here>',
-    bitlyToken:'<bit.ly Token Here>'
+    botToken:'1945998216:AAGDIF7TefC6Rgjb0fzvqd0M5fmqxgOD28s',
+    bitlyToken:'40b253c3acff20f9afbee9ae37f6a5509a484309'
 };
